53540e534a1a5c465349289ae023cc2361b141ff
sairre <jsairre@163.com>
2016-06-02 22:06:12 +0800

    to distribute the endian of big or little

---
+++ ByteOrdeer/ByteOrdeer/example.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <iostream>
+void byteorder();
+union 
+{
+	short value;
+	char union_bytes[sizeof(short)];
+}test;
+void byteorder()
+{
+	
+	
+	test.value = 0x0102;
+
+	if ((test.union_bytes[0] == 1 && test.union_bytes[1] == 2))
+	{
+		printf("big endian\n");
+	}
+	else if ((test.union_bytes[0] == 2 && test.union_bytes[1] == 1))
+	{
+		printf("little endian\n");
+	}
+	else
+	{
+		printf("unknown...\n");
+	}
+
+}
+
+int main(void)
+{
+	byteorder();
+
+	system("pause");
+
+	return 0;
+}
